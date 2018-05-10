8ad610f4b29e31c77828bbbf7fec158d59d73d37
Laura Gosney <lauragosney82@gmail.com>
2018-05-10 21:57:37 +0100

    ready for heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -23,6 +23,7 @@ Rails.application.configure do
   # Apache or NGINX already handles this.
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
+
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = Uglifier.new(harmony: true)
 
